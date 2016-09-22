73a598e5eb2459f2b42ab8ca06809e6ebea0164f
Proulx <am50250@logti-a3412-23.ens.ad.etsmtl.ca>
2016-09-22 10:28:05 -0400

    Delete everything and restart with swift

---
+++ Cosomer/Cosomer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cosomer
+//
+//  Created by Proulx, Olivier on 16-09-22.
+//  Copyright © 2016 Proulx, Olivier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
