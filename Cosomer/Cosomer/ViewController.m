c48cd98e67e497dcb00c85874e852a15a817e0fc
Proulx <am50250@logti-a3412-23.ens.ad.etsmtl.ca>
2016-09-22 10:11:51 -0400

    Add all input connections

--- Cosomer/Cosomer/ViewController.m
+++ Cosomer/Cosomer/ViewController.m
@@ -9,9 +9,76 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-- (IBAction)nextPeriode:(UIButton *)sender;
 
-@property (weak, nonatomic) IBOutlet UILabel *periode;
+@property (weak, nonatomic) IBOutlet UILabel *period;
+
+
+
+@property (weak, nonatomic) IBOutlet UITextField *team1_name;
+@property (weak, nonatomic) IBOutlet UILabel *team1_score;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j1_number;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j2_number;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j3_number;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j4_number;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j5_number;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j1_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j2_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j3_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j4_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j5_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j1_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j2_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j3_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j4_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team1_j5_lastname;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j1_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j2_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j3_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j4_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j5_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j1_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j2_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j3_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j4_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team1_j5_assist;
+@property (weak, nonatomic) IBOutlet UITextField *team1_input_goal;
+@property (weak, nonatomic) IBOutlet UITextField *team1_input_assist1;
+@property (weak, nonatomic) IBOutlet UITextField *team1_input_assist2;
+- (IBAction)team1_line_submit:(UIButton *)sender;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *team2_name;
+@property (weak, nonatomic) IBOutlet UILabel *team2_score;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j1_number;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j2_number;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j3_number;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j4_number;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j5_number;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j1_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j2_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j3_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j4_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j5_firstname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j1_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j2_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j3_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j4_lastname;
+@property (weak, nonatomic) IBOutlet UITextField *team2_j5_lastname;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j1_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j2_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j3_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j4_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j5_goal;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j1_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j2_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j3_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j4_assist;
+@property (weak, nonatomic) IBOutlet UILabel *team2_j5_assist;
+@property (weak, nonatomic) IBOutlet UITextField *team2_input_goal;
+@property (weak, nonatomic) IBOutlet UITextField *team2_input_assist1;
+@property (weak, nonatomic) IBOutlet UITextField *team2_input_assist2;
+- (IBAction)team2_line_submit:(UIButton *)sender;
+
 
 @end
 
@@ -27,7 +94,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)nextPeriode:(UIButton *)sender {
-    self.periode.text = @"Periode 2";
+- (IBAction)team1_line_submit:(UIButton *)sender {
 }
+
 @end
