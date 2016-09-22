5e4e00b2861a7390cb50c1d0c3fd3d24d17640dd
f4xxf4xx <felix.hubert.1@etsmtl.net>
2016-09-22 08:27:24 -0400

    Add xcode files

---
+++ Cosomer/Cosomer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cosomer
+//
+//  Created by Felix Hubert on 2016-09-22.
+//  Copyright © 2016 Les Hiboux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cosomer/Cosomer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cosomer
+//
+//  Created by Felix Hubert on 2016-09-22.
+//  Copyright © 2016 Les Hiboux. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
